fee8cef69ec4e2aa62c12c4dd5feb7c62e0989dc
Artemis fowl <realgarcilaso@outlook.com>
2018-05-04 11:30:07 -0500

    01 agrega login social authentication
    Inicio de sesion con gituhub listo.- por fin, la otra vez no me salia pq
    la parte no podia llamar a la funcion o clase baaaaaa. pero esta vez si
    se pudo.

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+
+
+
+        'github' => [
+            'client_id' => 'd0b49cf4dcd959ff4e3d',
+            'client_secret' => '76330c27da1b9f510581dfcec083bbf52187ee3b',
+            'redirect_uri' => 'http://foro.test/github/redirect',
+            'scope' => [],
+        ],
+
+
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
